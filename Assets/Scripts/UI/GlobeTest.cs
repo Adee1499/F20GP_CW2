41c1cd326d195006cb041febdcd3648d70ef37fa
cade <65869535+ctrlcade@users.noreply.github.com>
2023-03-21 23:06:04 +0000

    UI Globes
    * UI Globes added as prefab
    
    * Test script included
    
    * TODO: Integrate with player stats/values

---
+++ Assets/Scripts/UI/GlobeTest.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GlobeTest : MonoBehaviour
+{
+    public Slider healthSlider;
+    public float refillSpeed;
+    public bool refilling;
+    
+    void Start()
+    {
+        healthSlider = GetComponent<Slider>();
+    }
+
+    void Update()
+    {
+        if(refilling) {
+            
+            // refill at defined speed
+            healthSlider.value = healthSlider.value + (refillSpeed * Time.deltaTime);
+            
+            // when globe full, refilling off
+            if(healthSlider.value == 1) {
+                refilling = false;   
+            }
+        }
+        
+        // left click to lose fluid
+        if(Input.GetMouseButtonDown (0)) {
+            healthSlider.value = healthSlider.value - 0.1f;   
+        }
+        
+        // R for refilling
+        if(Input.GetKeyDown(KeyCode.R)) {
+            refilling = true;   
+        }
+    }
+}
