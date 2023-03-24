2ab34f2a00768dc8fea74a7acdcd7323ae43fb9d
JamesBeach2 <james.beach.98@gmail.com>
2023-03-24 17:30:29 +0000

    incorporate loot into breakables, add rarity modifiers

--- Assets/Scripts/Breakable.cs
+++ Assets/Scripts/Breakable.cs
@@ -9,6 +9,10 @@ public class Breakable : MonoBehaviour
     public float breakRadius = 2.0f;
     public LayerMask layermask;
 
+    public int suggestedLevel = 1;
+    public int numberOfLoot = 1;
+    public float rarityModifier = 0.1f;
+
     private Outline outline;
 
     void Awake() 
@@ -21,6 +25,10 @@ public class Breakable : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown("f")){
+                Fracture();
+        }
+
         Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
         RaycastHit hit;
         if (Physics.Raycast (ray, out hit, 1000, layermask)) {
@@ -45,6 +53,9 @@ public class Breakable : MonoBehaviour
         foreach(Rigidbody body in fracturedPot.GetComponentsInChildren<Rigidbody>()) {
             body.AddExplosionForce(force ?? breakForce, transform.position, breakRadius, 3.0f);
         }
+
+        LootManager lootManager = FindObjectOfType<LootManager>();
+        lootManager.DropLoot(transform.position, numberOfLoot, suggestedLevel, rarityModifier);
 
         Destroy(gameObject);
     }
--- Assets/Scripts/Inventory System/ScriptableObjects/EquipmentItem.cs
+++ Assets/Scripts/Inventory System/ScriptableObjects/EquipmentItem.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class EquipmentItem : InventoryItem
 {
     public int defenseValue = 5;
+    public LootRarity lootRarity;
 
     public void Awake()
     {
