0af0d84afc008bb5a0619b8fe7f396e5aa819620
cade <cade7brown@gmail.com>
2023-04-01 19:53:45 +0100

    Main Menu initial work done

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void GoToGame() {
+        // SceneManager.LoadScene("");
+    }
+    
+    public void QuitGame() {
+        Application.Quit();
+    }
+    
+}
